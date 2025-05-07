0907f8e0445a0170b3b722e8d17f593efd9cab83
Cheescue <alexandar.drobnjakk@gmail.com>
2025-05-07 11:23:20 +0200

    finished

--- js/kevin.js
+++ js/kevin.js
@@ -1,4 +1,11 @@
 function toggleBox(box) {
     box.classList.toggle("expanded");
 }
- 
+
+function toggleMenu() {
+    console.log("Menu toggled!");  // För att kontrollera att funktionen körs
+    var menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('active');
+}
+
+
